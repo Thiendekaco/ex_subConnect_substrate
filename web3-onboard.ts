c8c9d6fa8c99560f02f35bdab09b6d9ef18605bd
Thiendekaco <thienguo@gmail.com>
2024-05-30 20:04:16 +0700

    update open bit

--- web3-onboard.ts
+++ web3-onboard.ts
@@ -1,21 +1,54 @@
 
-import { init } from '@subwallet-connect/react'
+import { init } from '@subwallet-connect/react';
 import walletConnectPolkadotModule from '@subwallet-connect/walletconnect-polkadot';
-import subwalletPolkadotModule from '@subwallet-connect/subwallet-polkadot';
-import polkadotVaultModule from '@subwallet-connect/polkadot-vault';
+import ledgerPolkadot from "@subwallet-connect/ledger-polkadot";
+import metamaskSDK from '@subwallet-connect/metamask';
 import talismanModule from '@subwallet-connect/talisman';
 import polkadot_jsModule from '@subwallet-connect/polkadot-js';
-import ledgerPolkadot from "@subwallet-connect/ledger-polkadot";
+import subwalletPolkadotModule from '@subwallet-connect/subwallet-polkadot';
+import polkadotVaultModule from '@subwallet-connect/polkadot-vault';
+import walletConnectModule from '@subwallet-connect/walletconnect';
+import OpenBitModule from '@subwallet-connect/openbit';
+import {TransactionHandlerReturn} from "@subwallet-connect/core/dist/types";
+
+
+// Example key • Replace with your infura key
+const INFURA_KEY = '302750fdd8644da3b50aa6daa0b89336'
+
+
+const ledgerPolkadot_ = ledgerPolkadot();
+
+
 
 
 const walletConnectPolkadot = walletConnectPolkadotModule({
-  projectId: 'd4a987cb0d1a746dbffd38890458e65c',
+  projectId: '59b5826141a56b204e9e0a3f7e46641d',
+  dappUrl: 'https://w3o-demo.subwallet.app/'
 })
-const subwalletPolkadotWalet = subwalletPolkadotModule();
-const polkadotVaultWallet = polkadotVaultModule();
-const talismanWallet = talismanModule();
+const metamaskSDKWallet = metamaskSDK({
+  options: {
+    extensionOnly: false,
+    i18nOptions: {
+      enabled: true
+    },
+    infuraAPIKey : INFURA_KEY,
+    dappMetadata: {
+      name: 'SubConnect'
+    }
+  }
+})
 const polkadotWallet = polkadot_jsModule();
-const ledgerPolkadot_ = ledgerPolkadot();
+const subwalletPolkadotWalet = subwalletPolkadotModule();
+const talismanWallet = talismanModule();
+const polkadotVaultWallet = polkadotVaultModule();
+const openBitWallet = OpenBitModule();
+const walletConnect = walletConnectModule({
+  projectId: '59b5826141a56b204e9e0a3f7e46641d',
+  dappUrl: 'https://w3o-demo.subwallet.app/'
+})
+const subwalletWCIds = '9ce87712b99b3eb57396cc8621db8900ac983c712236f48fb70ad28760be3f6a';
+const coinbaseWCIds = 'fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa';
+const metamaskWCIds = 'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96'
 
 export default init({
   theme: "dark",
@@ -23,16 +56,80 @@ export default init({
     autoConnectLastWallet : true,
     autoConnectAllPreviousWallet : true
   },
-  accountCenter: {
-    desktop : {
-      enabled: true,
-      minimal : true
-    },
-    mobile: {
-      enabled: true
-    }
+
+  wcConfigOption: {
+    projectId: '59b5826141a56b204e9e0a3f7e46641d',
+    explorerRecommendedWalletIds: [subwalletWCIds, coinbaseWCIds, metamaskWCIds]
   },
-  projectId : 'd4a987cb0d1a746dbffd38890458e65c',
+
+  // An array of wallet modules that you would like to be presented to the user to select from when connecting a wallet.
+  wallets: [
+    subwalletPolkadotWalet,
+    walletConnectPolkadot,
+    walletConnect,
+    metamaskSDKWallet,
+    ledgerPolkadot_,
+    talismanWallet,
+    polkadotWallet,
+    openBitWallet,
+    polkadotVaultWallet
+  ],
+  // An array of Chains that your app supports
+  chains: [
+    {
+      id: '0x507',
+      rpcUrl: 'https://rpc.api.moonbase.moonbeam.network',
+      label: 'Moonbase Alpha',
+      token:  'DEV',
+      namespace : 'evm',
+      decimal: 18
+    },
+    {
+      // hex encoded string, eg '0x1' for Ethereum Mainnet
+      id: '0x1',
+      // string indicating chain namespace. Defaults to 'evm' but will allow other chain namespaces in the future
+      namespace: 'evm',
+      // the native token symbol, eg ETH, BNB, MATIC
+      token: 'ETH',
+      // used for display, eg Ethereum Mainnet
+      label: 'Ethereum',
+      // used for network requests
+      rpcUrl: `https://ethereum.publicnode.com`,
+      decimal: 18
+    },
+    {
+      id: '0x504',
+      rpcUrl: 'https://rpc.api.moonbeam.network',
+      label: 'Moonbeam',
+      token: 'GLMR',
+      namespace : 'evm',
+      decimal: 18
+    },
+    {
+      id: '0x505',
+      rpcUrl: 'https://rpc.api.moonriver.moonbeam.network',
+      label: 'Moonriver',
+      namespace: 'evm',
+      token : 'MOVR',
+      decimal: 18
+    },
+    {
+      id: '0x250',
+      rpcUrl: 'https://astar.api.onfinality.io/public',
+      label: 'Astar - EVM',
+      namespace : 'evm',
+      token:  'ASTR',
+      decimal: 18
+    },
+    {
+      id: '0x150', // 336
+      rpcUrl: 'https://shiden.public.blastapi.io',
+      label: 'Shiden',
+      token: 'SDN',
+      namespace : 'evm',
+      decimal: 18
+    }
+  ],
 
 
   chainsPolkadot:[
@@ -78,23 +175,12 @@ export default init({
       token: 'KSM',
       blockExplorerUrl: 'assethub-kusama.subscan.io'
     }
-
   ],
 
-  // An array of wallet modules that you would like to be presented to the user to select from when connecting a wallet.
-  wallets: [
-    walletConnectPolkadot,
-    subwalletPolkadotWalet,
-    polkadotVaultWallet,
-    ledgerPolkadot_,
-    talismanWallet,
-    polkadotWallet,
-  ],
-  // An array of Chains that your app supports
-  chains: [],
   appMetadata: {
     // The name of your dApp
-    name: 'SubWallet Connect',
+    name: 'SubConnect',
+
 
     description: 'Demo app for SubWalletConnect V2',
     // The url to a getting started guide for app
@@ -114,8 +200,28 @@ export default init({
     // Optional - but allows for dapps to require users to agree to TOS and privacy policy before connecting a wallet
     agreement: {
       version: '1.0.0',
-      termsUrl: 'https://www.blocknative.com/terms-conditions',
-      privacyUrl: 'https://www.blocknative.com/privacy-policy'
+      termsUrl: 'https://docs.subwallet.app/main/privacy-and-security/terms-of-use',
+    }
+  },
+  notify: {
+    desktop: {
+      enabled: true,
+      transactionHandler: (transaction) :TransactionHandlerReturn => {
+        if (transaction.eventCode === 'txConfirmed') {
+          return {
+            autoDismiss: 0
+          }
+        }
+        // if (transaction.eventCode === 'txPool') {
+        //   return {
+        //     type: 'hint',
+        //     message: 'Your in the pool, hope you brought a towel!',
+        //     autoDismiss: 0,
+        //     link: `https://goerli.etherscan.io/tx/${transaction.hash}`
+        //   }
+        // }
+      },
+      position: 'topCenter'
     }
   }
 })
