6673477dbec23bd7e02365ecf9cfa1e9fd1276b6
1% <139972251+Thiendekaco@users.noreply.github.com>
2024-07-09 12:48:12 +0700

    Update web3-onboard.ts

--- web3-onboard.ts
+++ web3-onboard.ts
@@ -89,47 +89,13 @@ export default init({
 
   chainsPolkadot:[
     {
-      id: '0xe143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423e' ,
+      id: '0xe005f23803ac50e8f6f8e62695d1ce109e1d68aa36c1cd2cfd15340213f3423f' ,
       token: 'WND',
       decimal : 12,
       label: 'Westend',
       blockExplorerUrl: 'westend.subscan.io',
       namespace: 'substrate'
     },
-    {
-      id: '0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3',
-      namespace: 'substrate',
-      token: 'DOT',
-      label: 'Polkadot',
-      blockExplorerUrl: `polkadot.api.subscan.io`,
-      decimal: 10
-    },
-    {
-      id: '0x68d56f15f85d3136970ec16946040bc1752654e906147f7e43e9d539d7c3de2f',
-      label: 'Polkadot Asset Hub',
-      namespace: 'substrate',
-      decimal: 10,
-      token: 'DOT',
-      blockExplorerUrl: 'assethub-polkadot.subscan.io',
-    },
-
-    {
-      id: '0xb0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafe',
-      label: 'Kusama',
-      decimal: 12,
-      namespace: 'substrate',
-      token: 'KSM',
-      blockExplorerUrl: 'kusama.api.subscan.io'
-    },
-
-    {
-      id: '0x48239ef607d7928874027a43a67689209727dfb3d3dc5e5b03a39bdc2eda771a',
-      label: 'Kusama Asset Hub',
-      decimal: 12,
-      namespace: 'substrate',
-      token: 'KSM',
-      blockExplorerUrl: 'assethub-kusama.subscan.io'
-    }
   ],
 
   appMetadata: {
